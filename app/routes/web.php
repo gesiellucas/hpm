21331595ba90277058e963706a4964375042dd3d
Gesiel Lucas <gesiel.lucas@hotmail.com>
2023-02-09 12:52:20 -0300

    feat: install predis and configure

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,15 +15,20 @@ use Illuminate\Support\Facades\Redis;
 
 Route::get('/', function () {
 
+    $redis = Redis::connection();
 
-$redis = Redis::connection();
+    try {
+        $redis->set('test_key', 'test_value');
+        $value = $redis->get('test_key');
+        if ($value === 'test_value') {
+            echo 'Redis connection successful!';
+        } else {
+            echo 'Redis connection failed: Unexpected value retrieved';
+        }
+    } catch (Exception $e) {
+        echo 'Redis connection failed: ' . $e->getMessage();
+    }
 
-try {
-    $redis->ping();
-    echo 'Redis connection successful!';
-} catch (Exception $e) {
-    echo 'Redis connection failed: ' . $e->getMessage();
-}
     dd('teste');
     return view('welcome');
 });
