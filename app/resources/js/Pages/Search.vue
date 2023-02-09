b178bb370bad0e0abf879e9326745a4f652ffbe9
Gesiel Lucas <gesiel.lucas@hotmail.com>
2023-02-09 20:48:38 -0300

    style: init bring color for the app

---
+++ app/resources/js/Pages/Search.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+
+<template>
+    <main class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <h1>Pesquisar paciente</h1>
+                <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example" />
+            </div>
+        </div>
+
+        <div class="card">
+            Paciente: Hugo Idade: 3 actions
+            <button class="btn btn-sm btn-danger">EDITAR</button>
+            <button class="btn btn-sm btn-danger">APAGAR</button>
+        </div>
+    </main>
+</template>
