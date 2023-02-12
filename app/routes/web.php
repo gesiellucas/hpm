03dd0c5aac973041d3a94864d7f9c0d8c9cc41ce
Gesiel Lucas <gesiel.lucas@hotmail.com>
2023-02-12 14:11:48 -0300

    feat: create routes

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PatientController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -17,22 +18,19 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Home');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/pesquisa', function(){
+    return Inertia::render('Search');
 });
+
+Route::get('/cadastro', [PatientController::class, 'createPatient']);
+Route::post('/create-patient', [PatientController::class, 'create']);
+
+Route::get('/users', [PatientController::class, 'list']);
+
+Route::get('/editar/{id}', [PatientController::class, 'update']);
+
 
 require __DIR__.'/auth.php';
