f9f289fd8d29047004ce5de3cbe487c1bcecc75a
Gesiel Lucas <gesiel.lucas@hotmail.com>
2023-02-12 14:08:32 -0300

    feat: Home Page view

---
+++ app/resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <main class="text-center p-4">
+        <h3 class="">Teste de habilidades</h3>
+        <p class="w-50 mx-auto"><small>Desenvolver um cadastro de pacientes para validar meus conhecimentos na área, como esse é o único requisito irei criar como se estivesse trabalhando em um projeto real. Sendo assim apresento o sistema para o <b>Hospital Pediátrico Mineiro</b>. (nome fictício).</small></p>
+        <div class="px-4 py-5 my-3 text-center">
+
+            <h1 class="display-5 fw-bold">Hospital Pediátrico Mineiro</h1>
+            <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4">O sistema do Hospital Pediátrico Mineiro auxilia médicos e funcionários a encontrar informações dos pacientes de forma rápida e precisa.</p>
+                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <a href="/pesquisa" type="button" class="btn btn-primary btn-lg px-4 gap-3">Pesquisar pacientes</a>
+                    <a href="/cadastro" type="button" class="btn btn-outline-secondary btn-lg px-4">Cadastrar Pacientes</a>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</template>
